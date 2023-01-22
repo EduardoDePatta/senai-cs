1a4d2851d00a7f3efb5a5c93539f3033ba109df6
Eduardo De Patta <eduardo.depatta@gmail.com>
2023-01-22 18:28:16 -0300

    fix: removido valores fixos do Program)

--- Program.cs
+++ Program.cs
@@ -2,38 +2,10 @@
 
 // Endereço
 Endereco novoEnderecoPessoaFisica = new Endereco();
-
-novoEnderecoPessoaFisica.logradouro = "Rua Verona";
-novoEnderecoPessoaFisica.bairro = "Pagani";
-novoEnderecoPessoaFisica.complemento = "Casa";
-novoEnderecoPessoaFisica.numero = 111;
-novoEnderecoPessoaFisica.endComercial = false;
-
 // Pessoa Física
 PessoaFisica novaPf = new PessoaFisica();
-
-novaPf.dataNasc = new DateTime(1992,05,23);
-novaPf.nome = "Eduardo";
-novaPf.rendimento = 6000;
-
-bool dataNascValidada = novaPf.ValidarDataNascimento(novaPf.dataNasc);
-
-float impostoCalculadoPessoaFisica = novaPf.CalcularImposto(novaPf.rendimento);
-
-Console.WriteLine(@$"
-Nome: {novaPf.nome}
-Maior de Idade: {dataNascValidada}
---ENDEREÇO--
-Rua: {novoEnderecoPessoaFisica.logradouro}
-Número: {novoEnderecoPessoaFisica.numero}
-Bairro: {novoEnderecoPessoaFisica.bairro}
-Complemento: {novoEnderecoPessoaFisica.complemento}
-Endereço Comercial?: {novoEnderecoPessoaFisica.endComercial}
-Valor do imposto com rendimento de {novaPf.rendimento.ToString("C")}: {impostoCalculadoPessoaFisica.ToString("C")}
-");
-
-
 // Pessoa Jurídica
 PessoaJuridica novaPj = new PessoaJuridica();
-novaPj.rendimento = 15000;
-float impostoCalculadoPessoaJuridica = novaPj.CalcularImposto(novaPj.rendimento);
+
+
+Utils.start();
